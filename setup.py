2ec53b4dd30cf6feee7345c45e49254a787f8fe9
Sotiris Lamprinidis <sot.lampr@gmail.com>
2016-01-07 16:52:08 +0200

    Setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='theano_wrapper',
+      version='0.0a',
+      description='Utilities for machine learning applications with theano',
+      url='https://github.com/sotlampr/theano-wrapper',
+      author='Sotiris Lamprinidis',
+      author_email='sot.lampr@gmail.com',
+      license='MIT',
+      packages=['theano_wrapper'],
+      install_requires=['numpy', 'scipy', 'theano', 'scikit-learn',
+                        'nose', 'coverage'],
+      zip_safe=False)
+
